09c82be3e12d2c5ae1cd9773c9f89e0937d7d77c
Kira Roberts <rosehill1788@protonmail.com>
2019-09-13 12:06:32 -0700

    fix typos in template literals in api link, burn down broken promise logic

--- src/main.js
+++ src/main.js
@@ -18,16 +18,9 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      if(body.data.length === 0) {
-        $('#noMatch').text("Your search returned no results. Refine your search.")
-        body.data.forEach() {
-          $('#getDoctorNameOutput').text(`Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`)
-          $('#getDoctorPhoneOutput').text(`Phone Number: ${body.data[i].practices[0].phones[0].number}`)
-        }
-      } else {
-        function(error) {
-          $('#errorOutput').text(`${error.message}`);
-        }
+      console.log(body);
+      if (body.data.length === 0) {
+        $('#outputCard').text("Your search returned no results. Refine your search.")
       }
     });
   });
