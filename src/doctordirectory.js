48dafd7e564cf1618062eead37607595a77aa4a9
Kira Roberts <rosehill1788@protonmail.com>
2019-09-13 12:46:34 -0700

    change .text to .append to display more than one result

--- src/doctordirectory.js
+++ src/doctordirectory.js
@@ -1,15 +1,15 @@
-export class doctorDirectory {
-  getDoctor() {
+export class DoctorDirectory {
+  getDoctor(name, ailment) {
     return new Promise(function(resolve,reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}query=${illness}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${ailment}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { doctorDirectory } from './doctordirectory';
+import { DoctorDirectory } from './doctordirectory';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,12 +9,12 @@ $(document).ready(function() {
   $('#inputForm').submit(function(event) {
     event.preventDefault();
     let name = $('#inputName').val();
-    let illness = $('#inputIllness').val();
+    let ailment = $('#inputIllness').val();
     $('#inputName').val("");
     $('#inputIllness').val("");
 
-    let doctorDirectory = new doctorDirectory();
-    let promise = doctorDirectory.getDoctor(name, illness);
+    let doctorDirectory = new DoctorDirectory();
+    let promise = doctorDirectory.getDoctor(name, ailment);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
@@ -22,6 +22,14 @@ $(document).ready(function() {
       if (body.data.length === 0) {
         $('#outputCard').text("Your search returned no results. Refine your search.")
       }
+
+      for (let i = 0; i <body.data.length; i++) {
+        $('#getDoctorNameOutput').append(`Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`)
+      }
+      // body.data.forEach(function(doctor) {
+      //   $('#getDoctorNameOutput').text(`Name: ${doctor.profile.first_name} ${doctor.profile.last_name}`)
+      //   $('#getDoctorPhoneOutput').text(`Phone Number: ${doctor.practices[0].phones[0].number}`)
+      // })
     });
   });
 });
