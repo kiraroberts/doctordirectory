e1976ff4e462f95e626badd23dd1a3ee86ab32ec
Kira Roberts <rosehill1788@protonmail.com>
2019-09-13 10:49:37 -0700

    add api call logic

--- src/doctordirectory.js
+++ src/doctordirectory.js
@@ -1,15 +1,17 @@
-export function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
-    }
+export class doctorDirectory {
+  getDoctor() {
+    return new Promise(function(resolve,reject) {
+      let request = new XHMLttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}query=${inputIllness}&&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
-  return output;
 }
