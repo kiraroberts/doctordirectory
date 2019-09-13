562020d18219cfe4bce29c3cc6a0170132bfc536
Kira Roberts <rosehill1788@protonmail.com>
2019-09-13 11:28:10 -0700

    fix typo in api url

--- src/doctordirectory.js
+++ src/doctordirectory.js
@@ -2,7 +2,7 @@ export class doctorDirectory {
   getDoctor() {
     return new Promise(function(resolve,reject) {
       let request = new XHMLttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}query=${inputIllness}&&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}query=${inputIllness}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
